40bfdf063df85d39c9bdb23fe9c524c6d2b72963
frede122 <fredemanduoliveira@hotmail.com>
2019-03-10 19:18:25 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("stickymenu");
+var sticky = stickymenu.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+      stickymenu.classList.add("yes-sticky");
+      stickymenu.classList.remove("no-sticky");
+      stickymenu.classList.add("bg-dark");
+     
+      
+  } else {
+    stickymenu.classList.add("no-sticky");
+    stickymenu.classList.remove("yes-sticky");
+    stickymenu.classList.remove("bg-dark");
+
+  }
+}
