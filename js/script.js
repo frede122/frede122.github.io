6ea2324b0e1873184a56d3f0b6b99bd42a7b8025
frede122 <fredemanduoliveira@hotmail.com>
2019-03-26 15:46:09 -0300

    criação de animações da id area-3

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 window.onscroll = function() {myFunction()};
 
+
 var navbar = document.getElementById("stickymenu");
 var sticky = stickymenu.offsetTop;
+var area = document.getElementById("area-3");
+var about = document.getElementById("aboutme");
 
 function myFunction() {
-  if (window.pageYOffset >= sticky+450) {
+  if (window.pageYOffset >= sticky) {
       stickymenu.classList.add("yes-sticky");
       stickymenu.classList.remove("no-sticky");
       stickymenu.classList.add("bg-dark");
@@ -16,4 +19,19 @@ function myFunction() {
     stickymenu.classList.remove("bg-dark");
 
   }
+    if(window.pageYOffset >= area.offsetTop - 400){
+        about.classList.remove("opacityObj");
+        descr.classList.remove("opacityObj");
+        about.classList.add("bounceInRight");
+        descr.classList.add("bounceInUp");
+        
+    }
+    if(window.pageYOffset <= area.offsetTop - 600){
+        about.classList.remove("bounceInRight");
+        descr.classList.remove("bounceInUp");
+        about.classList.add("opacityObj");
+        descr.classList.add("opacityObj");
+    }
 }
+
+
