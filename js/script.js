cea6b88e89ca436aec0a877d0f75d4e9ba992923
frede122 <fredemanduoliveira@hotmail.com>
2019-03-25 22:47:09 -0300

    alteração básica do layout

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var navbar = document.getElementById("stickymenu");
 var sticky = stickymenu.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset >= sticky+450) {
       stickymenu.classList.add("yes-sticky");
       stickymenu.classList.remove("no-sticky");
       stickymenu.classList.add("bg-dark");
