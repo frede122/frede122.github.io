cda09f115b54b7e92f3c8417ac72fae2d01131be
frede122 <fredemanduoliveira@hotmail.com>
2020-01-12 02:42:54 -0300

    Fade in elements

--- js/script.js
+++ js/script.js
@@ -1,51 +1,56 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() { scrollFunction() };
 
-
-function stickyBar(){
-
-    
-}
-
-function stickyArea(){
-    var area = document.getElementById("question-section");
+function scrollFunction() {
+    var areaQuestion = document.getElementById("question-section");
     var about = document.getElementById("aboutme");
-    var imgAbout = document.getElementById("img-user");
-    if(window.pageYOffset + (screen.height * 0.7 ) >= area.offsetTop){
-        
+    var information = document.getElementById("information");
+    var informationLogo = document.getElementById("information-logo");
+    var informationDescr = document.getElementById("information-descr");
+    var glyphiconStats =  document.getElementById("glyphicon-stats");
+    var glyphiconWrench =  document.getElementById("glyphicon-wrench");
+    var glyphiconCloud =  document.getElementById("glyphicon-cloud");
+    var glyphiconPencil =  document.getElementById("glyphicon-pencil");
+    var glyphiconQrCode =  document.getElementById("glyphicon-qrcode");
+    var glyphiconPhone =  document.getElementById("glyphicon-phone");
+
+
+    // Menu Area 
+    const altura = window.innerHeight;
+    if (window.pageYOffset > stickymenu.offsetTop) {
+      stickymenu.classList.add("yes-sticky","bg-dark-transp");
+      stickymenu.classList.remove("no-sticky");
+
+    } else {
+        stickymenu.classList.add("no-sticky");
+        stickymenu.classList.remove("yes-sticky");
+        stickymenu.classList.remove( "bg-dark-transp");
+    }
+
+    // Icons Area
+    if(window.pageYOffset > (glyphiconWrench.offsetTop - altura)) { glyphiconWrench.classList.add("fadeIn", "animated", "slow"); }
+    if(window.pageYOffset > (glyphiconStats.offsetTop - altura)) { glyphiconStats.classList.add("fadeIn", "animated", "slow", "delay-05s"); }
+    if(window.pageYOffset > (glyphiconCloud.offsetTop - altura)) { glyphiconCloud.classList.add("fadeIn", "animated", "slow", "delay-1s"); }
+    if(window.pageYOffset > (glyphiconPencil.offsetTop - altura)) { glyphiconPencil.classList.add("fadeIn", "animated", "slow"); }
+    if(window.pageYOffset > (glyphiconQrCode.offsetTop - altura)) { glyphiconQrCode.classList.add("fadeIn", "animated", "slow", "delay-05s"); }
+    if(window.pageYOffset > (glyphiconPhone.offsetTop - altura)) { glyphiconPhone.classList.add("fadeIn", "animated", "slow", "delay-1s"); }
+    
+
+    // About Area
+    if(window.pageYOffset  > areaQuestion.offsetTop){   
         about.classList.remove("opacityObj");
         descr.classList.remove("opacityObj");
         about.classList.add("fadeInRight");
         descr.classList.add("fadeInUp");
         hist1.classList.add("fadeInLeftBig");
         hist2.classList.add("fadeInLeftBig");
-        
     }
-    else{
-        about.classList.remove("fadeInRight");
-        descr.classList.remove("fadeInUp");
-        about.classList.add("opacityObj");
-        descr.classList.add("opacityObj");
-        hist1.classList.remove("fadeInLeftBig");
-        hist2.classList.remove("fadeInLeftBig");
+
+    // Information 
+    if(window.pageYOffset > (information.offsetTop - altura + 100 )){
+        informationLogo.classList.add("fadeInRight");
+        informationDescr.classList.add("fadeInLeft");
     }
-}
 
-
-function scrollFunction() {
-    if (window.pageYOffset > stickymenu.offsetTop) {
-
-      stickymenu.classList.add("yes-sticky","bg-dark-transp");
-      stickymenu.classList.remove("no-sticky");
-       
-     
-      
-    } else {
-        stickymenu.classList.add("no-sticky");
-        stickymenu.classList.remove("yes-sticky");
-        stickymenu.classList.remove( "bg-dark-transp");
-    }
-    stickyArea();
-    
 }
 
 
